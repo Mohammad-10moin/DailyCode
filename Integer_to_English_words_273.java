cec5aab5433d507483ab1e4ad2944db147656280
Mohammad-10moin <moinuddin10code@gmail.com>
2024-08-07 21:52:50 +0530

    August 7th question -1ms Solution

--- Integer_to_English_words_273.java
+++ Integer_to_English_words_273.java
@@ -1,38 +1,36 @@
 public class Integer_to_English_words_273 {
-    public static void main(String[] args) {
-        int num=104000000;
-        if (num == 0) {
-            System.out.println("Zero");
-        }
-        int count = 0, i = num;
-        String ans = "";
-        while (i > 0) {
-            count++;
-            if (count == 2 && i%1000!=0 && i!=10000 && i!=100000 && i!=1000000) {
-                ans = " Thousand " + ans;
-            } else if (count == 3 && i!=1000) {
-                ans = " Million " + ans;
-            } else if (count == 4 ) {
-                ans = " Billion " + ans;
-            }
-            ans = word(i % 1000) + ans;
+        private static final String[] belowTwenty = {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine",
+                "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
+        private static final String[] tens = {"", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
 
-            i = i / 1000;
+        public static String numberToWords(int num) {
+            if (num == 0) {
+                return "Zero";
+            }
+            return helper(num);
         }
-        System.out.println((ans.trim().replaceAll("\\s+", " ")));
-    }
-    public static String word(int num) {
-        String[] ones = { "", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine" };
-        String[] teens = { "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen",
-                "Eighteen", "Nineteen" };
-        String[] tens = { "", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety" };
-        if (num < 10) {
-            return ones[num];
-        } else if (num < 20) {
-            return teens[num - 10];
-        } else if (num < 100) {
-            return tens[num / 10] + " " + ones[num % 10];
+
+        private static String helper(int num) {
+            StringBuilder result = new StringBuilder();
+            if (num < 20) {
+                result.append(belowTwenty[num]);
+            } else if (num < 100) {
+                result.append(tens[num / 10]).append(" ").append(belowTwenty[num % 10]);
+            } else if (num < 1000) {
+                result.append(helper(num / 100)).append(" Hundred ").append(helper(num % 100));
+            } else if (num < 1000000) {
+                result.append(helper(num / 1000)).append(" Thousand ").append(helper(num % 1000));
+            } else if (num < 1000000000) {
+                result.append(helper(num / 1000000)).append(" Million ").append(helper(num % 1000000));
+            } else {
+                result.append(helper(num / 1000000000)).append(" Billion ").append(helper(num % 1000000000));
+            }
+            return result.toString().trim();
         }
-        return ones[num / 100] + " Hundred " + word(num % 100);
+
+    public static void main(String[] args) {
+        int num=1000000;
+        String ans=numberToWords(num);
+        System.out.println(ans);
     }
 }
